6cf47de5fba29dc47244f39e6a2f8e0582995335
Jason Beres <jasonberes@gmail.com>
2023-05-06 10:44:20 -0400

    OpenFin News Feed 2 updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TraderAppV2Component } from './trader-app-v2/trader-app-v2.component';
 import { IgxNavigationDrawerModule, IgxIconModule, IgxTabsModule, IgxGridModule, IgxActionStripModule } from 'igniteui-angular';
+import { IgxCategoryChartModule } from 'igniteui-angular-charts';
 import { FormsModule } from '@angular/forms';
 import { MainComponent } from './main/main.component';
-import { IgxCategoryChartModule } from 'igniteui-angular-charts';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -23,9 +23,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     IgxNavigationDrawerModule,
     IgxIconModule,
+    IgxCategoryChartModule,
     FormsModule,
     IgxTabsModule,
-    IgxCategoryChartModule,
     IgxGridModule,
     IgxActionStripModule,
     HttpClientModule
